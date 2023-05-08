1da419da5041cf04944541b30a7f1875d36aa4e5
HAMMED ADEWUMI AFENIFERE <104014722+HAMMED007@users.noreply.github.com>
2023-05-08 17:30:38 +0300

    Backend Springboot

---
+++ src/main/java/com/example/ecommerce/Controller/ProductController.java
@@ -0,0 +1,69 @@
+package com.example.ecommerce.Controller;
+
+import com.example.ecommerce.Entity.Product;
+import com.example.ecommerce.Repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+    //Add New Product
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        System.out.println("Request Coming........");
+        Product createdProduct = productRepository.save(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdProduct);
+    }
+    //Get All Products
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+    //get by id
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Optional<Product> product = productRepository.findById(id);
+        return product.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    //Update the product
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(
+            @PathVariable Long id,
+            @RequestBody Product updatedProduct
+    ) {
+        Optional<Product> existingProduct = productRepository.findById(id);
+        if (existingProduct.isPresent()) {
+            updatedProduct.setId(id);
+            Product savedProduct = productRepository.save(updatedProduct);
+            return ResponseEntity.ok(savedProduct);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    //deleting the product
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        Optional<Product> existingProduct = productRepository.findById(id);
+        if (existingProduct.isPresent()) {
+            productRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
---
+++ src/main/java/com/example/ecommerce/Entity/Product.java
@@ -0,0 +1,78 @@
+package com.example.ecommerce.Entity;
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private int price;
+
+    @Column(name = "releasedate")
+    private String releaseDate;
+
+    // Constructor, getters and setters
+
+    public Product(Long id, String name, String description, int price, String releaseDate) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.releaseDate = releaseDate;
+    }
+
+    public Product() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+}
